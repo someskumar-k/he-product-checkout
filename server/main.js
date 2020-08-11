f7d4f490c18a61b72afebc82ac5effebeac8bfa4
Somes Kumar K <someshkumarasamy@gmail.com>
2020-08-11 12:29:56 +0530

    git push

--- server/main.js
+++ server/main.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express();
 
 
-const port = process.env.PORT || 3051
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(bodyParser.json({limit:'5mb'}));
